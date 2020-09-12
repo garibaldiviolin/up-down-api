b9acf06b7af84b530920ae7c89277b2347b695e6
neto <garibaldi.neto@olist.com>
2020-09-12 13:23:37 -0300

    Check if unzip and terraform are present

--- start.sh
+++ start.sh
@@ -1,7 +1,17 @@
 #/bin/bash
-sudo apt-get update
-sudo apt-get install -y unzip
 
-wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
-unzip terraform_0.11.13_linux_amd64.zip
+# if unzip is not present, install it
+if ! [ -x "$(command -v unzip)" ]; then
+    sudo apt-get update
+    sudo apt-get install -y unzip
+fi
 
+# if terraform is not present, install it
+if ! [ -e ./terraform ]; then
+    rm -Rf ./terraform*
+    wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
+    unzip terraform_0.11.13_linux_amd64.zip
+fi
+
+export PATH=$PATH:./terraform
+./terraform --help
