4dc5f2cfef113bd25b7421a3d9f3f18ae9b24350
neto <garibaldiviolin@gmail.com>
2020-09-12 14:52:25 -0300

    Read AWS credentials from user prompt

--- start.sh
+++ start.sh
@@ -1,5 +1,17 @@
 #/bin/bash
 
+# If aws_credentials file is not present, ask and save the
+# user's AWS credentials to be used by Terraform
+if ! [ -e ./aws_credentials ]; then
+    echo "Please inform your AWS credentials"
+    read -p "aws_access_key_id: " aws_access_key_id
+    read -p "aws_secret_access_key: " aws_secret_access_key
+
+    echo "[default]" > aws_credentials
+    echo "aws_access_key_id = $aws_access_key_id" >> aws_credentials
+    echo "aws_secret_access_key = $aws_secret_access_key" >> aws_credentials
+fi
+
 # if unzip is not present, install it
 if ! [ -x "$(command -v unzip)" ]; then
     sudo apt-get update
