51607ccddcc04b71740138874bfbf720769f249f
neto <garibaldiviolin@gmail.com>
2020-09-13 22:57:17 -0300

    Add Terraform RDS variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,7 @@ variable "access_key" {}
 variable "secret_key" {}
 variable "region" {}
 variable "public_key_path" {}
+variable "rds_instance_identifier" {}
+variable "database_name" {}
+variable "database_password" {}
+variable "database_user" {}
