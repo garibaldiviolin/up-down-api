810eaa8e0a5017ebbe333e5ef71a98273fd2a145
neto <garibaldiviolin@gmail.com>
2020-10-07 21:26:21 -0300

    Enable DNS for VPC to allow public access to RDS

--- project.tf
+++ project.tf
@@ -12,6 +12,8 @@ provider "template" {
 # Create a VPC to launch our instances into.
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
 
   tags {
     Name = "terraform-example-vpc"
